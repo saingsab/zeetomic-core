799ab535e36361a3fcb9f47653d4cc88cecc27cf
Mr Ayoung <saing.sab@gmail.com>
2020-03-18 19:38:56 +0700

    fixed reward not send

--- src/zeetomic_core/operation/pay.clj
+++ src/zeetomic_core/operation/pay.clj
@@ -89,23 +89,20 @@
 (defn reward!
   [branches-name destination amount txid]
   (let [sender (branches/get-branches-by-name conn/db {:BRANCHES_NAME branches-name})]
-    (println "Check balance.....")
-    (if (= true (enought-balance? (get (users/get-users-by-id conn/db {:ID (get sender :created_by)}) :wallet) "ZTO" 0.0002))
-      (try
-        (println "Start FEE.....")
-        (fee (ed/decrypt (get (users/get-seed-by-id conn/db {:ID (get sender :created_by)}) :seed)))
-        (println "Start Rewarding.....")
-        (client/post (str (get env :sendpayment))
-                     {:form-params {:senderKey (ed/decrypt (get (users/get-seed-by-id conn/db {:ID (get sender :created_by)}) :seed))
-                                    :assetCode (get sender :asset_code)
-                                    :destination destination
-                                    :amount (str amount)
-                                    :memo "Reward!"}
-                      :content-type :json})
-
-        (catch Exception ex
-          (writelog/tx-log! (str "FAILDED : REWARD! From " (get sender :branches_name) " To : " (.getMessage ex)))))
-      {:error {:message "Something went wrong on our end"}})))
+    (try
+      (println (str "Start Rewarding....." amount))
+      (println (client/post (str (get env :sendpayment))
+                            {:form-params {:senderKey (ed/decrypt (get (users/get-seed-by-id conn/db {:ID (get sender :created_by)}) :seed))
+                                           :assetCode (get sender :asset_code)
+                                           :destination destination
+                                           :amount (str amount)
+                                           :memo "Reward!"}
+                             :content-type :json}))
+      (println "Start FEE.....")
+      (fee (ed/decrypt (get (users/get-seed-by-id conn/db {:ID (get sender :created_by)}) :seed)))
+      (println "Rewarding Finished!")
+      (catch Exception ex
+        (writelog/tx-log! (str "FAILDED : REWARD! From " (get sender :branches_name) " To : " (.getMessage ex)))))))
 
 
 (defn portforlio [token]
